73e001f34f467a138d4ea357a76fd0c6b3193819
brenermr <brenercaique0806@gmail.com>
2022-03-08 23:23:44 -0300

    GamePlayKotlin

--- app/src/main/java/com/ciandt/gameplay/ui/ScheduleMatchesActivity.kt
+++ app/src/main/java/com/ciandt/gameplay/ui/ScheduleMatchesActivity.kt
@@ -1,12 +1,21 @@
 package com.ciandt.gameplay.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ciandt.gameplay.R
+import com.ciandt.gameplay.databinding.ActivityScheduleMatchesBinding
 
 class ScheduleMatchesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityScheduleMatchesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_schedule_matches)
+        binding = ActivityScheduleMatchesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backScreen.setOnClickListener {
+            val intent = Intent(this, HelloPlayer::class.java)
+            startActivity(intent)
+        }
     }
 }
